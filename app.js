c544d3db832b21a6b2a2ced3e1ffac594180b785
Vijeth <poojaryvijeth239@gmail.com>
2025-09-22 01:35:44 +0530

    Sixth Commit , added My name to display in browser corrected version

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const express = require('express');
 const app = express();
 
 app.get('/health', (req, res) => {
+  res.json({
+    status: 'ok',
+    version: process.env.GIT_SHA || 'dev'
+  });
+});
+
+app.get('/', (req, res) => {
   res.send(`
-    <h1 style="color:blue;">Hello, I'm Vijeth </h1>
+    <h1 style="font-family:sans-serif; color:blue">Hello, I'm Vijeth 🚀</h1>
     <pre>${JSON.stringify({
       status: 'ok',
       version: process.env.GIT_SHA || 'dev'
     }, null, 2)}</pre>
   `);
-});
-
-app.get('/', (req, res) => {
-  res.redirect('/health');
 });
 
 module.exports = app;
