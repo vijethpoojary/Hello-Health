9c7c96acfc0e41d534f402878102759ac6a3fbea
Vijeth <poojaryvijeth239@gmail.com>
2025-09-22 00:55:58 +0530

    Second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/health', (req, res) => {
+app.get('/', (req, res) => {
   res.json({
     status: 'ok',
     version: process.env.GIT_SHA || 'dev'
